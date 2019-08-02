722a3b869ce7fd4a6ec0697165686b15af54c297
Alex Powarzynski <alexpowarzynski@gmail.com>
2019-08-02 13:52:58 -0700

    Clear button added

--- assets/js/alex.js
+++ assets/js/alex.js
@@ -44,97 +44,112 @@ $("#connotation").on("click", function () {
         $slangBox.empty();
         slangShowing = false;
     }else{
-    slangShowing = true;
-    let search = $(".userInput").val().trim();
-    $.ajax({
-        url: "https://api.urbandictionary.com/v0/define?term=" + search,
-        method: "GET"
-    }).then(response => {
+        slangShowing = true;
+        let search = $(".userInput").val().trim();
+        $.ajax({
+            url: "https://api.urbandictionary.com/v0/define?term=" + search,
+            method: "GET"
+        }).then(response => {
 
-        if ($slangBox.children().length === 0) {
+            if ($slangBox.children().length === 0) {
 
-            let $contentBox = $("<div>").addClass("contentBox");
-            let $header = $("<h2>").text(search.charAt(0).toUpperCase() + search.slice(1));
-            let $defList = $("<ol>");
-            let queryLimit = Math.round(response.list.length / 2);
-            for (let i = 0; i < queryLimit; i++) {
-                let $listItem = $("<li>").text(response.list[i].definition);
-                $defList.append($listItem);
+                let $contentBox = $("<div>").addClass("contentBox");
+                let $header = $("<h2>").text(search.charAt(0).toUpperCase() + search.slice(1));
+                let $defList = $("<ol>");
+                let queryLimit = Math.round(response.list.length / 2);
+                for (let i = 0; i < queryLimit; i++) {
+                    let $listItem = $("<li>").text(response.list[i].definition);
+                    $defList.append($listItem);
+                }
+
+                $contentBox.append($header, $defList);
+                $slangBox.append($contentBox);
+
             }
-
-            $contentBox.append($header, $defList);
-            $slangBox.append($contentBox);
-
-        }
-    });
-}});
+        });
+    }});
 
 $("#translate").on("click", function() {
     let $translateBox = $(".translateBox");
     if (translateShowing){
 
-    $translateBox.empty();
-    translateShowing = false;
+        $translateBox.empty();
+        translateShowing = false;
 
     }else if(!translateShowing){
         $translateBox.empty();
         translateShowing = true;
-    let languages = [
-        "English",
-        "Spanish",
-        "French",
-        "Chinese",
-        "Japanese"
-    ];
-    let languageCodes = [
-        "en",
-        "es",
-        "fr",
-        "zh",
-        "ja"
-    ];
-    let $contentBox = $("<div>").addClass("contentBox");
-    let $translated = $("<div>").addClass("translatedBox");
-    let $dropDown = $("<select class= 'form-control language' id='exampleFormControlSelect1'></select>");
-    for (i = 0; i < languages.length; i++){
-        let $option = $("<option>" + languages[i] + "</option>").val(languageCodes[i]);
-        $dropDown.append($option);
-    }
-    let $translate = $("<button>Translate</button>").addClass("btn translateButton");
-
-    $contentBox.append($dropDown, $translated, $translate);
-    $translateBox.append($contentBox);
-
-    }
-$(".translateButton").on("click", function(){
-    var source = "auto";
-    var target = $(".language").val().trim();
-    var input = $(".userInput").val().trim();
-    $.ajax({
-        method:'GET',
-        url: 'https://api-platform.systran.net/translation/text/translate?key=948fb53e-0398-41b8-9b8b-5adad715d36a',
-        dataType: 'text',
-        data: {
-            source: source,
-            target: target,
-            input: input
-        },
-        success: function(data) {
-            if (typeof data === 'string')
-                try {
-                    data = JSON.parse(data);
-                    $(".translatedBox").text(data.outputs[0].output);
-                } catch (exp) {
-
-                }
-        },
-        error: function(xhr, status, err) {
+        let languages = [
+            "English",
+            "Spanish",
+            "French",
+            "Chinese",
+            "Japanese"
+        ];
+        let languageCodes = [
+            "en",
+            "es",
+            "fr",
+            "zh",
+            "ja"
+        ];
+        let $contentBox = $("<div>").addClass("contentBox");
+        let $translated = $("<div>").addClass("translatedBox");
+        let $dropDown = $("<select class= 'form-control language' id='exampleFormControlSelect1'></select>");
+        for (i = 0; i < languages.length; i++){
+            let $option = $("<option>" + languages[i] + "</option>").val(languageCodes[i]);
+            $dropDown.append($option);
         }
+        let $translate = $("<button>Translate</button>").addClass("btn translateButton");
+
+        $contentBox.append($dropDown, $translated, $translate);
+        $translateBox.append($contentBox);
+
+    }
+    $(".translateButton").on("click", function(){
+        var source = "auto";
+        var target = $(".language").val().trim();
+        var input = $(".userInput").val().trim();
+        $.ajax({
+            method:'GET',
+            url: 'https://api-platform.systran.net/translation/text/translate?key=948fb53e-0398-41b8-9b8b-5adad715d36a',
+            dataType: 'text',
+            data: {
+                source: source,
+                target: target,
+                input: input
+            },
+            success: function(data) {
+                if (typeof data === 'string')
+                    try {
+                        data = JSON.parse(data);
+                        $(".translatedBox").text(data.outputs[0].output);
+                    } catch (exp) {
+
+                    }
+            },
+            error: function(xhr, status, err) {
+            }
+        });
     });
-});
 
 });
 
+function clear(){
+    let $defineBox = $(".defineBox");
+    $defineBox.empty();
+    let $slangBox = $(".slangBox");
+    $slangBox.empty();
+    let $translateBox = $(".translateBox");
+    $translateBox.empty();
+    $(".userInput").val("");
+    defineShowing = false;
+    slangShowing = false;
+    translateShowing = false;
+}
+$("#clear").on("click", function(){
+    clear();
+});
 
 
 
